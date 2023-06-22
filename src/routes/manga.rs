663804abde3ac0c81656be692e832c564df54b1f
Bennet <bennetleins@gmx.de>
2023-06-22 22:59:07 +0200

    checks if the session cookie exist before getting its value to prevent errors

--- src/routes/manga.rs
+++ src/routes/manga.rs
@@ -14,6 +14,19 @@ pub fn get_all_mangas() -> Json<Vec<(u32, String, u32, bool, bool)>> {
 }
 
 #[post("/Manga", format = "json", data="<manga>")]
-pub fn create_manga(manga: Json<CreateManga>) -> Json<u64> {
-    Json(db_layer::manga::add(manga))
+pub fn create_manga(manga: Json<CreateManga>, cookie: &CookieJar<'_>) -> Json<u64> {
+    if cookie.get("session").is_none() {
+        return Json(0)
+    };
+
+    let cookie_session = cookie.get("session").unwrap().value();
+    println!("Cookie: {:?}", cookie_session);
+    let user_id = db_layer::user::get_id_by_session(cookie_session.to_string());
+    let is_admin = db_layer::user::is_admin(user_id);
+
+    if is_admin {
+        return Json(db_layer::manga::add(manga))
+    };
+
+    return Json(0)
 }
