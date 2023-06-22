e833634cd6d42f0ed12b15c7ea9a986713462d7b
BennetLe <bennetleins@gmx.de>
2023-06-23 00:03:33 +0200

    Now prevents the user from creating lists with the same name

--- src/db_layer/user_list.rs
+++ src/db_layer/user_list.rs
@@ -7,6 +7,7 @@ use crate::URL;
 use crate::{db_layer, services};
 
 use services::user_list::CreateUserList;
+use crate::services::user_list::AddMangaToList;
 
 pub fn get_all() -> Vec<(u32, String, bool, u32)> {
     let mut conn = db_layer::connection::connect();
@@ -19,13 +20,22 @@ pub fn create(
     new_user_list: Json<CreateUserList>,
     cookie: &CookieJar<'_>
 ) -> u64 {
+    let name = new_user_list.name.to_owned();
+    let mut conn = db_layer::connection::connect();
+    let query = "SELECT id FROM user_list WHERE name = ?";
+    let result :Vec<u32> = conn.exec(query, (name.clone(), )).unwrap();
+
+    if result.is_empty() {
+        return 0;
+    }
+
+
     let cookie_session = cookie.get("session").unwrap().value();
     let user_id = db_layer::user::get_id_by_session(cookie_session.to_string());
 
     if user_id >= 1 {
         let mut conn = db_layer::connection::connect();
         let query = "INSERT INTO user_list (name, user_id) VALUES (?, ?)";
-        let name = new_user_list.name.to_owned();
         let result = conn.exec_iter(query, (name, user_id)).unwrap();
         return result.affected_rows();
     };
@@ -34,5 +44,9 @@ pub fn create(
 }
 
 pub fn add_maga_to_list(
+    manga_list: Json<AddMangaToList>
+) -> u8 {
 
-)
+
+    return 0;
+}
