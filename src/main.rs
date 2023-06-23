ec9b76e379804896ebd01409f7b5dbca79c35d95
Bennet <bennetleins@gmx.de>
2023-06-23 14:05:35 +0200

    Added an API endpoint to change the Manga chapter

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use mysql::{Pool, PooledConn};
 use crate::routes::options::all_options;
-use crate::routes::manga::{get_all_mangas, create_manga};
+use crate::routes::manga::{get_all_mangas, create_manga, update_manga_chapters};
 use crate::routes::user_list::{get_all_user_lists, create_user_list, add_manga_to_list, update_current_chapter, get_mangas_from_list};
 use crate::routes::logic::{get_all_user_mangas};
 use crate::routes::user::{login, logout};
@@ -31,9 +31,8 @@ fn rocket() -> _ {
                 add_manga_to_list,
                 update_current_chapter,
                 get_mangas_from_list,
+                update_manga_chapters
         ])
 }
 
-// TODO: Add an Endpoint to see all list + mangas in a specific list
-// TODO: Add an Endpoint for Admins to change the chapter count of a Manga
 // TODO: Add an Endpoint to create an Account
